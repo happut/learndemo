9c73c39f049c8df2d6da0354246f36ded3dfcc6b
bjwangfei3 <bjwangfei3@corp.netease.com>
2017-07-24 18:09:08 +0800

    learn lambda expression

--- learndemocollect/src/main/java/Test.java
+++ learndemocollect/src/main/java/Test.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class Test {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        for (int i = 0; i < 100; i++) {
+            list.add(i + "");
+        }
+
+//        list.forEach(s -> {
+//            System.out.println(s);
+//        });
+
+        list.forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println(s);
+            }
+        });
+    }
 }
