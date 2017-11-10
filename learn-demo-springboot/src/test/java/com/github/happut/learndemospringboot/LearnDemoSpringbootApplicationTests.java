bb2ce8f3991393ce2632220c4e9138a2eccea685
wangfei <happut@163.com>
2017-11-10 20:43:13 +0800

    修改测试方法注解

--- learn-demo-springboot/src/test/java/com/github/happut/learndemospringboot/LearnDemoSpringbootApplicationTests.java
+++ learn-demo-springboot/src/test/java/com/github/happut/learndemospringboot/LearnDemoSpringbootApplicationTests.java
@@ -5,11 +5,13 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockServletConfig;
+import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
-@SpringBootConfiguration(classes = MockServletConfig.class)
-@SpringBootTest
+@SpringBootTest(classes = MockServletContext.class)
+@WebAppConfiguration
 public class LearnDemoSpringbootApplicationTests {
 
 	@Test
