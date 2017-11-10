0faba6b024ce3b72f2ffc29be46e0037a4a14cf4
wangfei <happut@163.com>
2017-11-10 11:29:56 +0800

    增加springboot项目研究内容

---
+++ learn-demo-springboot/src/test/java/com/github/happut/learndemospringboot/LearnDemoSpringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.happut.learndemospringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearnDemoSpringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
