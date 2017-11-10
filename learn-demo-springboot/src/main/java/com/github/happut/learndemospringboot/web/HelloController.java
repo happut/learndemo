65f9d2c4ed6dcc9afb99121da61b1afe498270a1
wangfei <happut@163.com>
2017-11-10 19:12:53 +0800

    增加springboot项目研究内容

---
+++ learn-demo-springboot/src/main/java/com/github/happut/learndemospringboot/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.github.happut.learndemospringboot.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+}
--- learn-demo-springboot/src/test/java/com/github/happut/learndemospringboot/LearnDemoSpringbootApplicationTests.java
+++ learn-demo-springboot/src/test/java/com/github/happut/learndemospringboot/LearnDemoSpringbootApplicationTests.java
@@ -2,10 +2,13 @@ package com.github.happut.learndemospringboot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockServletConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@SpringBootConfiguration(classes = MockServletConfig.class)
 @SpringBootTest
 public class LearnDemoSpringbootApplicationTests {
 
