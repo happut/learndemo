cccfe60260bbd95663de0fa05eb7a14539b5675d
wangfei <happut@163.com>
2018-02-01 17:26:24 +0800

    计算耗时示例

--- learnhbasedemo/src/main/java/Test.java
+++ learnhbasedemo/src/main/java/Test.java
@@ -1,12 +1,15 @@
+import com.google.common.base.Stopwatch;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class Test {
     public static void main(String[] args) throws IOException {
+        Stopwatch stopWatch = Stopwatch.createStarted();
+
         Element body = Jsoup.connect("https://movie.douban.com/subject/26665065/output_card").get().body();
 
         String vote = body.select(".rating_sum > .rating_people > span").first().text();
@@ -21,6 +24,11 @@ public class Test {
         }
         System.out.println(sum * 2 / 100.0);
 
-        java.util.UUID.randomUUID().toString().replace("-","");
+        stopWatch.stop();
+        System.out.println(stopWatch.elapsed(TimeUnit.MILLISECONDS) / 1000.0);
+
+        java.util.UUID.randomUUID().toString().replace("-", "");
+
+
     }
 }
