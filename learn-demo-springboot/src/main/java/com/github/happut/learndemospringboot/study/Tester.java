fd1b4677a5012f5566435bd1e2cceef4736c6ad1
wangfei <happut@163.com>
2018-09-07 13:58:36 +0800

    orange

--- learn-demo-springboot/src/main/java/com/github/happut/learndemospringboot/study/Tester.java
+++ learn-demo-springboot/src/main/java/com/github/happut/learndemospringboot/study/Tester.java
@@ -1,5 +1,13 @@
 package com.github.happut.learndemospringboot.study;
 
+import com.google.common.collect.Lists;
+import org.mortbay.util.ajax.JSON;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Tester {
     public static void main(String[] args) {
         Calculator add = new Calculator() {
@@ -20,5 +28,9 @@ public class Tester {
         System.out.println(add2.operate(1, 2));
 
         System.out.println(((Calculator) (a, b) -> a + b).operate(1, 2));
+
+        List<Person> people = Lists.newArrayList(new Person(1, "aa"), new Person(2, "bb"), new Person(3, "cc"));
+        Map<Integer, String> collect = people.stream().collect(Collectors.toMap(e -> e.getId(), e -> e.getName()));
+        System.out.println(JSON.toString(collect));
     }
 }
