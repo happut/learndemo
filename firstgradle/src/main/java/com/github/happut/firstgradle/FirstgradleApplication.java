d82d7375d3068e0689d1bd82b47ccd1de3e0544e
wangfei <happut@163.com>
2019-01-29 11:23:24 +0800

    add gradle

---
+++ firstgradle/src/main/java/com/github/happut/firstgradle/FirstgradleApplication.java
@@ -0,0 +1,14 @@
+package com.github.happut.firstgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstgradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstgradleApplication.class, args);
+    }
+
+}
+
