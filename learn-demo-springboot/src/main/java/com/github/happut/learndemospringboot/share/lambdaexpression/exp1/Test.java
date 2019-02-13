85826eda38a143f875845f775939c85c0b23a019
wangfei <happut@163.com>
2019-02-13 16:24:11 +0800

    add gradle

--- learn-demo-springboot/src/main/java/com/github/happut/learndemospringboot/share/lambdaexpression/exp1/Test.java
+++ learn-demo-springboot/src/main/java/com/github/happut/learndemospringboot/share/lambdaexpression/exp1/Test.java
@@ -5,7 +5,9 @@ import com.google.common.collect.Lists;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Test {
     public static void main(String[] args) {
@@ -18,5 +20,8 @@ public class Test {
         });
 
         Collections.sort(list, (a, b) -> a.compareTo(b));
+
+        Supplier<Double> random = Math::random;
+        Stream.generate(() -> 2).limit(20).forEach(System.out::println);
     }
 }
