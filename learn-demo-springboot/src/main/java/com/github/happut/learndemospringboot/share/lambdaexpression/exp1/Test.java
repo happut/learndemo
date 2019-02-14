985428096aa0bca94c0c48b4eac905a04e306e76
wangfei <happut@163.com>
2019-02-14 10:21:44 +0800

    lambda/Optional表达式

--- learn-demo-springboot/src/main/java/com/github/happut/learndemospringboot/share/lambdaexpression/exp1/Test.java
+++ learn-demo-springboot/src/main/java/com/github/happut/learndemospringboot/share/lambdaexpression/exp1/Test.java
@@ -5,9 +5,9 @@ import com.google.common.collect.Lists;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 public class Test {
     public static void main(String[] args) {
@@ -22,6 +22,22 @@ public class Test {
         Collections.sort(list, (a, b) -> a.compareTo(b));
 
         Supplier<Double> random = Math::random;
-        Stream.generate(() -> 2).limit(20).forEach(System.out::println);
+//        Stream.generate(() -> 2).limit(20).forEach(System.out::println);
+
+        int sum = IntStream.rangeClosed(1, 10).reduce((x, y) -> x + 2 * y).orElse(0);
+        System.out.println(sum);
+
+        String wangfei = "wangfei";
+        Optional<String> wangfeio = Optional.ofNullable(wangfei);
+        Optional<String> wangfei2 = Optional.ofNullable(null);
+
+        wangfeio.ifPresent(e -> System.out.println(e));
+        wangfei2.ifPresent(e -> System.out.println(e));
+        System.out.println(wangfeio.orElse("null String"));
+        System.out.println(wangfei2.orElse("null String"));
+        System.out.println(wangfei2.orElseGet(() -> "wangfei"));
+
+
+
     }
 }
