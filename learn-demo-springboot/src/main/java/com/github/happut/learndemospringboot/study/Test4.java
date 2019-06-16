2213f59d5acb40ef070e5ee8befd403b2a5f6a18
happut <happut@163.com>
2019-06-17 01:30:46 +0800

    test

--- learn-demo-springboot/src/main/java/com/github/happut/learndemospringboot/study/Test4.java
+++ learn-demo-springboot/src/main/java/com/github/happut/learndemospringboot/study/Test4.java
@@ -11,12 +11,15 @@ public class Test4 {
 
 //        String[] ids = new String[]{"YDJ0535Q4MQFWZXY", "YDJ0535Q4MNDXWYZ"};
 //        String[] ids = new String[]{"YDJ0535Q4M9BEWZX","YDJ0535Q4M9D9YZY"};
-        String[] ids = new String[]{"YDJ0535Q4M5E1WWX"};
+//        String[] ids = new String[]{"YDJ0535Q4M5E1WWX"};
+//        String[] ids = new String[]{"YDJ0535Q4N10VZXX","YDJ0535Q4N15MYZY"};
+//        String[] ids = new String[]{"YDJ0535Q4O14TXXY","YDJ0535Q4O18BYWY"};
+        String[] ids = new String[]{"YDJ0535Q4P141ZYZ","YDJ0535Q4PPQXXYX"};
 
         Arrays.stream(ids).forEach(id -> {
 
 
-            for (int i = 0; i < 300; i++) {
+            for (int i = 0; i < 100; i++) {
                 try {
                     String result = Jsoup.connect("https://c.m.163.com/reader/api/recommend/praise/add")
                             .header("Content-Type", "application/x-www-form-urlencoded")
